5b26ebe77361ac45e386fecaa030e480d9232948
Hannes Bochmann <hannes.bochmann@das-medienkombinat.de>
2012-03-05 14:51:40 +0000

    Tests gefixed

--- tests/class.ext_localconf_testcase.php
+++ tests/class.ext_localconf_testcase.php
@@ -37,15 +37,38 @@ tx_rnbase::load('tx_t3users_tests_Util');
  */
 class tx_t3users_tests_ext_localconf_testcase extends tx_phpunit_testcase {
 
+	/**
+	 *
+	 * @var string
+	 */
+	protected $sExpectedXClassPath;
+
+	/**
+	 *
+	 * @var string
+	 */
+	protected $sXClassPathConfig;
+
+	public function setUp() {
+		global $TYPO3_LOADED_EXT,$TYPO3_CONF_VARS;
+		$this->sExpectedXClassPath = PATH_site.$TYPO3_LOADED_EXT['t3users']['siteRelPath'].'xclasses/class.ux_tx_srfeuserregister_data.php';
+		$this->sXClassPathConfig = $TYPO3_CONF_VARS['FE']['XCLASS']['ext/sr_feuser_register/model/class.tx_srfeuserregister_data.php'];
+	}
+
 	public function tearDown() {
 		tx_t3users_tests_Util::setExtConfVar('useSaltedPasswordsWithSrFeUser', 0);
+		//backup zurück
+		global $TYPO3_CONF_VARS;
+		$TYPO3_CONF_VARS['FE']['XCLASS']['ext/sr_feuser_register/model/class.tx_srfeuserregister_data.php'] = $this->sXClassPathConfig;
 	}
 
 	public function testXClassIsNotRegisteredIfSrFeUserIsNotInstalledAndExtensionConfigNotActive() {
 		//unload sr_feuser_register
-		global $TYPO3_LOADED_EXT;
+		global $TYPO3_LOADED_EXT,$TYPO3_CONF_VARS;
 		if(isset($TYPO3_LOADED_EXT['sr_feuser_register']))
 			unset($TYPO3_LOADED_EXT['sr_feuser_register']);
+		//evtl. geladene xclasses löschen
+		$TYPO3_CONF_VARS['FE']['XCLASS']['ext/sr_feuser_register/model/class.tx_srfeuserregister_data.php'] = null;
 
 		$_EXTKEY = 't3users';
 		require(t3lib_extMgm::extPath('t3users', 'ext_localconf.php'));
@@ -55,23 +78,28 @@ class tx_t3users_tests_ext_localconf_testcase extends tx_phpunit_testcase {
 
 	public function testXClassIsNotRegisteredIfSrFeUserIsNotInstalledAndExtensionConfigActive() {
 		//unload sr_feuser_register
-		global $TYPO3_LOADED_EXT;
+		global $TYPO3_LOADED_EXT,$TYPO3_CONF_VARS;
 		if(isset($TYPO3_LOADED_EXT['sr_feuser_register']))
 			unset($TYPO3_LOADED_EXT['sr_feuser_register']);
+		//evtl. geladene xclasses löschen
+		$TYPO3_CONF_VARS['FE']['XCLASS']['ext/sr_feuser_register/model/class.tx_srfeuserregister_data.php'] = null;
 
 		tx_t3users_tests_Util::setExtConfVar('useSaltedPasswordsWithSrFeUser', 1);
 
 		$_EXTKEY = 't3users';
 		require(t3lib_extMgm::extPath('t3users', 'ext_localconf.php'));
 
-		$this->assertEmpty($TYPO3_CONF_VARS['FE']['XCLASS']['ext/sr_feuser_register/model/class.tx_srfeuserregister_data.php'],'XClass wurde doch registriert!');
+		//xclass darf nicht da sein
+		$this->assertNotEquals($this->sExpectedXClassPath,$TYPO3_CONF_VARS['FE']['XCLASS']['ext/sr_feuser_register/model/class.tx_srfeuserregister_data.php'],'XClass wurde doch registriert!');
 	}
 
 	public function testXClassIsNotRegisteredIfSrFeUserIsInstalledAndExtensionConfigNotActive() {
 		//load sr_feuser_register
-		global $TYPO3_LOADED_EXT;
+		global $TYPO3_LOADED_EXT,$TYPO3_CONF_VARS;
 		$TYPO3_LOADED_EXT['sr_feuser_register'] = array();
 		$TYPO3_LOADED_EXT['sr_feuser_register']['siteRelPath'] = 'typo3conf/ext/sr_feuser_register/';
+		//evtl. geladene xclasses löschen
+		$TYPO3_CONF_VARS['FE']['XCLASS']['ext/sr_feuser_register/model/class.tx_srfeuserregister_data.php'] = null;
 
 		if(!(t3lib_div::int_from_ver(t3lib_extMgm::getExtensionVersion('sr_feuser_register')) >= 2006003))
 			$this->markTestSkipped('sr_feuser_register muss in Version 2.6.3 installiert sein damit dieser Test ausgeführt werden kann!');
@@ -79,15 +107,18 @@ class tx_t3users_tests_ext_localconf_testcase extends tx_phpunit_testcase {
 		$_EXTKEY = 't3users';
 		require(t3lib_extMgm::extPath('t3users', 'ext_localconf.php'));
 
-		$this->assertEmpty($TYPO3_CONF_VARS['FE']['XCLASS']['ext/sr_feuser_register/model/class.tx_srfeuserregister_data.php'],'XClass wurde doch registriert!');
+		//xclass darf nicht da sein
+		$this->assertNotEquals($this->sExpectedXClassPath,$TYPO3_CONF_VARS['FE']['XCLASS']['ext/sr_feuser_register/model/class.tx_srfeuserregister_data.php'],'XClass wurde doch registriert!');
 	}
 
 	public function testXClassIsNotRegisteredIfSrFeUserIsInstalledInWrongVersionAndExtensionConfigActive() {
 		//load sr_feuser_register
-		global $TYPO3_LOADED_EXT;
+		global $TYPO3_LOADED_EXT,$TYPO3_CONF_VARS;
 		$key = 'sr_feuser_register';
 		$TYPO3_LOADED_EXT[$key] = array();
 		$TYPO3_LOADED_EXT[$key]['siteRelPath'] = 'typo3conf/ext/sr_feuser_register/';
+		//evtl. geladene xclasses löschen
+		$TYPO3_CONF_VARS['FE']['XCLASS']['ext/sr_feuser_register/model/class.tx_srfeuserregister_data.php'] = null;
 
 		if(!(t3lib_div::int_from_ver(t3lib_extMgm::getExtensionVersion('sr_feuser_register')) < 2006003))
 			$this->markTestSkipped('sr_feuser_register muss niedriger als in Version 2.6.3 installiert sein damit dieser Test ausgeführt werden kann!');
@@ -97,12 +128,13 @@ class tx_t3users_tests_ext_localconf_testcase extends tx_phpunit_testcase {
 		$_EXTKEY = 't3users';
 		require(t3lib_extMgm::extPath('t3users', 'ext_localconf.php'));
 
-		$this->assertEmpty($TYPO3_CONF_VARS['FE']['XCLASS']['ext/sr_feuser_register/model/class.tx_srfeuserregister_data.php'],'XClass wurde doch registriert!');
+		//xclass darf nicht da sein
+		$this->assertNotEquals($this->sExpectedXClassPath,$TYPO3_CONF_VARS['FE']['XCLASS']['ext/sr_feuser_register/model/class.tx_srfeuserregister_data.php'],'XClass wurde doch registriert!');
 	}
 
 	public function testXClassIsRegisteredIfSrFeUserIsInstalledInRightVersionAndExtensionConfigActive() {
 		//load sr_feuser_register
-		global $TYPO3_LOADED_EXT;
+		global $TYPO3_LOADED_EXT,$TYPO3_CONF_VARS;
 		$key = 'sr_feuser_register';
 		$TYPO3_LOADED_EXT[$key] = array();
 		$TYPO3_LOADED_EXT[$key]['siteRelPath'] = 'typo3conf/ext/sr_feuser_register/';
@@ -110,12 +142,16 @@ class tx_t3users_tests_ext_localconf_testcase extends tx_phpunit_testcase {
 		if(!(t3lib_div::int_from_ver(t3lib_extMgm::getExtensionVersion('sr_feuser_register')) >= 2006003))
 			$this->markTestSkipped('sr_feuser_register muss in Version 2.6.3 installiert sein damit dieser Test ausgeführt werden kann!');
 
+		//es darf keine andere xlcass geladen sein sonst unsere nicht genutzt wird!!!
+		//eine art sicherheitscheck
+		$this->assertEmpty($TYPO3_CONF_VARS['FE']['XCLASS']['ext/sr_feuser_register/model/class.tx_srfeuserregister_data.php'],'Es ist bereits eine XClass registriert für "$TYPO3_CONF_VARS[\'FE\'][\'XCLASS\'][\'ext/sr_feuser_register/model/class.tx_srfeuserregister_data.php\']" registriert. ('.$TYPO3_CONF_VARS['FE']['XCLASS']['ext/sr_feuser_register/model/class.tx_srfeuserregister_data.php'].') D.h. unsere wird nicht geladen!!! Es könnte z.B. srfeuserregister_t3secsaltedpw geladen sein welche sr_feuser_register >= 2.6.3 nicht geht!');
+
 		tx_t3users_tests_Util::setExtConfVar('useSaltedPasswordsWithSrFeUser', 1);
 
 		$_EXTKEY = 't3users';
 		require(t3lib_extMgm::extPath('t3users', 'ext_localconf.php'));
 
-		$this->assertEquals(PATH_site.$TYPO3_LOADED_EXT[$_EXTKEY]['siteRelPath'].'xclasses/class.ux_tx_srfeuserregister_data.php',$TYPO3_CONF_VARS['FE']['XCLASS']['ext/sr_feuser_register/model/class.tx_srfeuserregister_data.php'],'XClass wurde nicht registriert!');
+		$this->assertEquals($this->sExpectedXClassPath,$TYPO3_CONF_VARS['FE']['XCLASS']['ext/sr_feuser_register/model/class.tx_srfeuserregister_data.php'],'XClass wurde nicht registriert!');
 	}
 }
 ?>
