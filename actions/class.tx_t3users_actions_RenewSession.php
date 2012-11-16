a2999a1e0042b38517dc507a2929f23741ca3654
hbochmann <hannes.bochmann@das-medienkombinat.de>
2012-11-16 14:21:02 +0100

    fehlende dateien

---
+++ actions/class.tx_t3users_actions_RenewSession.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * 	@package TYPO3
+ *  @subpackage tx_t3users
+ *  @author Hannes Bochmann <hannes.bochmann@das-medienkombinat.de>
+ *
+ *  Copyright notice
+ *
+ *  (c) 2012 Hannes Bochmann <hannes.bochmann@das-medienkombinat.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ */
+
+/**
+ * benötigte Klassen einbinden
+ */
+require_once(t3lib_extMgm::extPath('rn_base') . 'class.tx_rnbase.php');
+tx_rnbase::load('tx_rnbase_action_BaseIOC');
+
+/**
+ * per Ajax aktuelle Seite in einem bestimmten Intervall aufrufen
+ * um ein automatisches Logout von TYPO3 zu verhindern.
+ * 
+ * @package TYPO3
+ * @subpackage tx_t3users
+ * @author Hannes Bochmann <hannes.bochmann@das-medienkombinat.de>
+ */
+class tx_t3users_actions_RenewSession extends tx_rnbase_action_BaseIOC {
+
+	/**
+	 * per Ajax aktuelle Seite in einem bestimmten Intervall aufrufen
+	 * um ein automatisches Logout von TYPO3 zu verhindern.
+	 *
+	 * @param tx_rnbase_parameters $parameters
+	 * @param tx_rnbase_configurations $configurations
+	 * @param array $viewData
+	 *
+	 * @return string error msg or null
+	 */
+	protected function handleRequest(&$parameters,&$configurations, &$viewdata){
+		$intervallInSeconds = 
+			$configurations->get($this->getConfId().'intervallInSeconds');
+			
+		$intervallInSeconds = $intervallInSeconds ? $intervallInSeconds : 300;
+		
+		$intervallInMilliSeconds = $intervallInSeconds * 1000;
+		
+		$GLOBALS['TSFE']->additionalHeaderData['tx_t3users_actions_RenewSession'] = 
+"<script type='text/javascript'>
+	RenewSession = {
+		loadCurrentPage: function(){
+			var xmlhttp;
+			
+			// code for IE7+, Firefox, Chrome, Opera, Safari
+			if (window.XMLHttpRequest) {
+			  	xmlhttp=new XMLHttpRequest();
+			// code for IE6, IE5
+			} else {
+			  	xmlhttp=new ActiveXObject('Microsoft.XMLHTTP');
+			}
+			xmlhttp.open('GET',window.location,true);
+			xmlhttp.send();
+		},
+		
+		loadCurrentPageInIntervall: function(intervall) {
+			window.setInterval('RenewSession.loadCurrentPage()', intervall);	
+		}
+	};
+
+	RenewSession.loadCurrentPageInIntervall($intervallInMilliSeconds);
+</script>";
+		
+		// wir brauchen kein template parsing
+		return '&nbsp;';
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getTemplateName() { return 'renewSession';}
+
+	/**
+	 * @return string
+	 */
+	public function getViewClassName() { return 'tx_rnbase_view_Base';}
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/t3users/actions/class.tx_t3users_actions_Login.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/t3users/actions/class.tx_t3users_actions_Login.php']);
+}
+
+?>
---
+++ tests/actions/class.tx_t3users_tests_actions_RenewSession_testcase.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * 	@package TYPO3
+ *  @subpackage tx_t3users
+ *  @author Hannes Bochmann <hannes.bochmann@das-medienkombinat.de>
+ *
+ *  Copyright notice
+ *
+ *  (c) 2012 Hannes Bochmann <hannes.bochmann@das-medienkombinat.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ */
+
+/**
+ * benötigte Klassen einbinden
+ */
+require_once(t3lib_extMgm::extPath('rn_base') . 'class.tx_rnbase.php');
+tx_rnbase::load('tx_t3users_actions_RenewSession');
+
+/**
+ * Testfälle für tx_t3users_actions_RenewSession
+ *
+ * @package TYPO3
+ * @subpackage tx_t3users
+ * @author Hannes Bochmann <hannes.bochmann@das-medienkombinat.de>
+ */
+class tx_t3users_tests_actions_RenewSession_testcase extends tx_phpunit_testcase {
+
+	/**
+	 * 
+	 */
+	protected function tearDown() {
+		if(isset($GLOBALS['TSFE']->additionalHeaderData['tx_t3users_actions_RenewSession']))
+			unset($GLOBALS['TSFE']->additionalHeaderData['tx_t3users_actions_RenewSession']);
+	}
+	
+	/**
+	 * @group unit
+	 */
+	public function testHandleRequestReturnsCorrectJavaScriptWithDefaultIntervall() {
+		$this->assertEmpty(
+			$GLOBALS['TSFE']->additionalHeaderData['tx_t3users_actions_RenewSession'],
+			'JavaScript bereits in Header Data gesetzt.'
+		);
+		
+		$this->executeAction();
+		
+		$expectedJavaScript = $this->getExpectedJavaScriptWithIntervall();		
+		
+		$this->assertEquals(
+			$expectedJavaScript,
+			$GLOBALS['TSFE']->additionalHeaderData['tx_t3users_actions_RenewSession'],
+			'Das Javascript ist nicht wie erwatet'
+		);
+	}
+	
+	/**
+	 * @group unit
+	 */
+	public function testHandleRequestReturnsCorrectJavaScriptWithIntervallSetByConfiguration() {
+		$this->assertEmpty(
+			$GLOBALS['TSFE']->additionalHeaderData['tx_t3users_actions_RenewSession'],
+			'JavaScript bereits in Header Data gesetzt.'
+		);
+		
+		$configurationsData = array(
+			'renewSession.' => array(
+    				'intervallInSeconds' => 400,
+			)
+		);
+		$this->executeAction($configurationsData);
+		
+		$expectedJavaScript = $this->getExpectedJavaScriptWithIntervall(400000);		
+		
+		$this->assertEquals(
+			$expectedJavaScript,
+			$GLOBALS['TSFE']->additionalHeaderData['tx_t3users_actions_RenewSession'],
+			'Das Javascript ist nicht wie erwatet'
+		);
+	}
+	
+	/**
+	 * @param array $configurationsData
+	 * 
+	 * @return void
+	 */
+	private function executeAction(array $configurationsData = array()) {
+		$action = tx_rnbase::makeInstance('tx_t3users_actions_RenewSession');
+		$configurations = tx_t3users_tests_Util::getConfigurations();
+		$parameters = tx_rnbase::makeInstance('tx_rnbase_parameters');
+		
+		if(!empty($configurationsData)) {
+			$cObj = t3lib_div::makeInstance('tslib_cObj');
+	    	$configurations->init($configurationsData, $cObj, 't3users', 't3users');
+		}
+		
+		$action->execute($parameters,$configurations);
+	}
+	
+	/**
+	 * @param integer $intervall
+	 * 
+	 * @return string
+	 */
+	private function getExpectedJavaScriptWithIntervall($intervall = 300000) {
+		return 
+"<script type='text/javascript'>
+	RenewSession = {
+		loadCurrentPage: function(){
+			var xmlhttp;
+			
+			// code for IE7+, Firefox, Chrome, Opera, Safari
+			if (window.XMLHttpRequest) {
+			  	xmlhttp=new XMLHttpRequest();
+			// code for IE6, IE5
+			} else {
+			  	xmlhttp=new ActiveXObject('Microsoft.XMLHTTP');
+			}
+			xmlhttp.open('GET',window.location,true);
+			xmlhttp.send();
+		},
+		
+		loadCurrentPageInIntervall: function(intervall) {
+			window.setInterval('RenewSession.loadCurrentPage()', intervall);	
+		}
+	};
+
+	RenewSession.loadCurrentPageInIntervall($intervall);
+</script>";		
+	}
+}
