690fdc65f708af81d52f15be45afc1162ec912e1
Ralf Urban <urban@das-medienkombinat.de>
2013-12-03 22:15:04 +0100

    AC264-46: DAM-Abhaengigkeit loesen, damit Extension auch ohne DAM eingesetzt werden kann.

--- views/class.tx_t3users_views_ListFeUsers.php
+++ views/class.tx_t3users_views_ListFeUsers.php
@@ -23,7 +23,7 @@
 ***************************************************************/
 
 require_once(t3lib_extMgm::extPath('rn_base') . 'class.tx_rnbase.php');
-require_once(t3lib_extMgm::extPath('dam') . 'lib/class.tx_dam_media.php');
+//require_once(t3lib_extMgm::extPath('dam') . 'lib/class.tx_dam_media.php');
 
 tx_rnbase::load('tx_rnbase_view_Base');
 tx_rnbase::load('tx_rnbase_util_ListBuilder');
