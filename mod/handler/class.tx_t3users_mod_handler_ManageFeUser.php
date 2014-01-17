b858610621bcc25a3a155d0a75eebc4cae191218
Thomas Reuleke <thomas.reuleke@das-medienkombinat.de>
2014-01-17 08:59:28 +0100

    Überflüssige Methode entfernt

--- mod/handler/class.tx_t3users_mod_handler_ManageFeUser.php
+++ mod/handler/class.tx_t3users_mod_handler_ManageFeUser.php
@@ -116,18 +116,6 @@ class tx_t3users_mod_handler_ManageFeUser extends tx_rnbase_mod_BaseModule {
 
 	public function handleRequest() {}
 
-	/**
-	 * Method to get a company searcher
-	 *
-	 * @param 	tx_rnbase_mod_IModule 	$mod
-	 * @param 	array 					$options
-	 * @return 	tx_mksearch_mod1_searcher_abstractBase
-	 */
-	protected function getSearcher(tx_rnbase_mod_IModule $mod, &$options) {
-		if(!isset($options['pid'])) $options['pid'] =  $mod->id;
-		return tx_rnbase::makeInstance('tx_t3users_mod_searcher_FeUser', $mod, $options);
-	}
-
 }
 
 if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/t3users/mod/handler/class.tx_t3users_mod_handler_ManageFeUser.php']) {
