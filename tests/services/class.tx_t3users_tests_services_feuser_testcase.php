d0ad94903cf58a8611a6c820b3647412c53f90b9
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2015-07-09 14:00:18 +0200

    [TASK] AC#235-3331 Datenbanktests refactored

--- tests/services/class.tx_t3users_tests_services_feuser_testcase.php
+++ tests/services/class.tx_t3users_tests_services_feuser_testcase.php
@@ -40,7 +40,7 @@ tx_rnbase::load('tx_t3users_services_feuser');
  * @author Michael Wagner <dev@dmk-ebusiness.de>
  */
 class tx_t3users_tests_services_feuser_testcase extends tx_phpunit_testcase {
-	
+
 	/**
 	 * @group unit
 	 * @dataProvider providerEmailDisable
@@ -52,7 +52,7 @@ class tx_t3users_tests_services_feuser_testcase extends tx_phpunit_testcase {
 					->emailDisable($sEMail)
 			);
 	}
-	
+
 	public function providerEmailDisable(){
 		return array( //array($sEMail, $sResult),
 				'Line: '.__LINE__ => array('ich@da.com', 'ich@@da.com'),
@@ -70,7 +70,7 @@ class tx_t3users_tests_services_feuser_testcase extends tx_phpunit_testcase {
 					->emailEnable($sEMail)
 			);
 	}
-	
+
 	public function providerEmailEnable() {
 		return array( //array($sEMail, $sResult),
 				'Line: '.__LINE__ => array('ich@da.com', 'ich@da.com'),
@@ -78,7 +78,7 @@ class tx_t3users_tests_services_feuser_testcase extends tx_phpunit_testcase {
 				'Line: '.__LINE__ => array('ich@@@@@@da.com', 'ich@da.com'),
 			);
 	}
-	
+
 	/**
 	 * @group unit
 	 */
@@ -86,18 +86,18 @@ class tx_t3users_tests_services_feuser_testcase extends tx_phpunit_testcase {
 		$feUserService = $this->getMock(
 			'tx_t3users_services_feuser', array('getRnBaseDbUtil')
 		);
-		
+
 		$feUserService->expects(($this->never()))
 			->method('getRnBaseDbUtil');
-			
+
 		$feUserRecord = array('uid' => 1);
 		$feUser = tx_rnbase::makeInstance('tx_t3users_models_feuser', $feUserRecord);
 		$groups = $feUserService->getFeGroups($feUser);
-		
+
 		$this->assertTrue(is_array($groups), 'kein array zurück gegeben');
 		$this->assertEmpty($groups, 'array nicht leer');
 	}
-	
+
 	/**
 	 * @group unit
 	 */
@@ -105,7 +105,7 @@ class tx_t3users_tests_services_feuser_testcase extends tx_phpunit_testcase {
 		$feUserService = $this->getMock(
 			'tx_t3users_services_feuser', array('getRnBaseDbUtil')
 		);
-		
+
 		$rnBaseDbUtil = $this->getMockClass(
 			'tx_rnbase_util_DB', array('doSelect')
 		);
@@ -119,21 +119,136 @@ class tx_t3users_tests_services_feuser_testcase extends tx_phpunit_testcase {
 			->method('doSelect')
 			->with('*', 'fe_groups')
 			->will($this->returnValue(array('testResult')));
-			
+
 		$feUserService->expects(($this->once()))
 			->method('getRnBaseDbUtil')
 			->will($this->returnValue($rnBaseDbUtil));
-			
+
 		$feUserRecord = array('uid' => 1, 'usergroup' => $usergroups);
 		$feUser = tx_rnbase::makeInstance('tx_t3users_models_feuser', $feUserRecord);
 		$groups = $feUserService->getFeGroups($feUser);
-		
+
 		$this->assertEquals(array('testResult'), $groups, 'gruppen falsch');
 	}
-}
 
-if (defined('TYPO3_MODE') && $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/t3users/tests/services/class.tx_t3users_tests_services_feuser_testcase.php']) {
-	include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/t3users/tests/services/class.tx_t3users_tests_services_feuser_testcase.php']);
-}
+	/**
+	 * @group unit
+	 * @expectedException tx_t3users_exceptions_User
+	 * @expectedExceptionMessage No user id given!
+	 */
+	public function testUpdateFeUserByConfirmstringThrowsExceptionIfNoUid() {
+		$feUserService = $this->getMock(
+			'tx_t3users_services_feuser', array('getRnBaseDbUtil')
+		);
+		$feUserService->expects(($this->never()))
+			->method('getRnBaseDbUtil');
 
-?>
+		$feUserService->updateFeUserByConfirmstring(0, '', array());
+	}
+
+	/**
+	 * @group unit
+	 * @expectedException tx_t3users_exceptions_User
+	 * @expectedExceptionMessage No user id given!
+	 */
+	public function testUpdateFeUserByConfirmstringThrowsExceptionIfUidIsNoInteger() {
+		$feUserService = $this->getMock(
+			'tx_t3users_services_feuser', array('getRnBaseDbUtil')
+		);
+		$feUserService->expects(($this->never()))
+			->method('getRnBaseDbUtil');
+
+		$feUserService->updateFeUserByConfirmstring('abc', '', array());
+	}
+
+	/**
+	 * @group unit
+	 * @expectedException tx_t3users_exceptions_User
+	 * @expectedExceptionMessage No confirmstring given!
+	 */
+	public function testUpdateFeUserByConfirmstringThrowsExceptionIfNoConfirmstring() {
+		$feUserService = $this->getMock(
+			'tx_t3users_services_feuser', array('getRnBaseDbUtil')
+		);
+		$feUserService->expects(self::never())
+			->method('getRnBaseDbUtil');
+
+		$feUserService->updateFeUserByConfirmstring(123, '', array());
+	}
+
+	/**
+	 * @group unit
+	 */
+	public function testUpdateFeUserByConfirmstringCallsDoUpdateCorrectIfUidAndConfirmstring() {
+		$feUserService = $this->getMock(
+			'tx_t3users_services_feuser', array('getRnBaseDbUtil')
+		);
+		$databaseUtility = $this->getMockClass(
+			'tx_rnbase_util_DB', array('doUpdate')
+		);
+		$data = array('city' => 'def');
+		$databaseUtility::staticExpects($this->once())
+			->method('doUpdate')
+			->with('fe_users', 'uid =	123 AND confirmstring = \'abc\'', $data, 0)
+			->will($this->returnValue('updateResult'));
+
+		$feUserService->expects(self::once())
+			->method('getRnBaseDbUtil')
+			->will(self::returnValue($databaseUtility));
+
+		self::assertEquals(
+			'updateResult',
+			$feUserService->updateFeUserByConfirmstring(123, 'abc', $data)
+		);
+	}
+
+	/**
+	 * @group unit
+	 */
+	public function testGetOnlineUsersCallsSearchCorrect(){
+		$feUserService = $this->getMock(
+			'tx_t3users_services_feuser', array('search')
+		);
+
+		$expectedFields = array(
+			'FESESSION.ses_userid' => array(OP_GT_INT => 0),
+			'CUSTOM' => '(ses_tstamp+86400 > unix_timestamp() OR is_online+86400 > unix_timestamp())'
+		);
+		$expectedOptions = array(
+			'pids' => '',
+			'count' => 1,
+			'distinct' => 1,
+		);
+		$feUserService->expects(self::once())
+			->method('search')
+			->with($expectedFields, $expectedOptions)
+			->will($this->returnValue('searchResult'));
+
+		self::assertEquals('searchResult', $feUserService->getOnlineUsers());
+	}
+
+	/**
+	 * @group unit
+	 */
+	public function testGetOnlineUsersCallsSearchCorrectIfOptionsGiven(){
+		$feUserService = $this->getMock(
+			'tx_t3users_services_feuser', array('search')
+		);
+
+		$expectedFields = array(
+			'FESESSION.ses_userid' => array(OP_GT_INT => 0),
+			'CUSTOM' => '(ses_tstamp+86400 > unix_timestamp() OR is_online+86400 > unix_timestamp())',
+			'FEUSER.pid' => array(OP_IN_INT => '1,2,3')
+		);
+		$expectedOptions = array(
+			'pids' => '1,2,3',
+			'distinct' => 1,
+		);
+		$feUserService->expects(self::once())
+			->method('search')
+			->with($expectedFields, $expectedOptions)
+			->will($this->returnValue('searchResult'));
+
+		self::assertEquals('searchResult', $feUserService->getOnlineUsers(array('pids' => '1,2,3')));
+	}
+}
