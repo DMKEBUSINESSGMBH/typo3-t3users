93324d418efc42349b112922507fb2f2c0837aa6
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2015-07-22 08:47:50 +0200

    [BUGFIX] fixed xClass inclusion for TYPO3 6.2

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,7 +5,21 @@ $GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['pro
 $GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tceforms.php']['getMainFieldsClass'][] = 'EXT:' . $_EXTKEY . '/hooks/class.tx_t3users_hooks_getMainFields.php:tx_t3users_hooks_getMainFields';
 
 // Anpassung tslib_feuserauth
-$GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['tslib/class.tslib_feuserauth.php'] = t3lib_extMgm::extPath($_EXTKEY).'xclasses/class.ux_tslib_feuserauth.php';
+if (class_exists('ux_tslib_feuserauth')) {
+	throw new LogicException(
+		'There allready exists an ux_tslib_feuserauth XCLASS!' .
+		' Remove the other XCLASS or or the user record won\'t be filled with the beforelastlogin column'
+	);
+}
+require_once t3lib_extMgm::extPath($_EXTKEY, 'xclasses/class.ux_tslib_feuserauth.php');
+if (tx_rnbase_util_TYPO3::isTYPO60OrHigher()) {
+	$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Frontend\\Authentication\\FrontendUserAuthentication'] = array(
+			'className' => 'ux_tslib_feuserauth'
+	);
+} else {
+	$GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['tslib/class.tslib_feuserauth.php'] =
+		t3lib_extMgm::extPath($_EXTKEY).'xclasses/class.ux_tslib_feuserauth.php';
+}
 
 $tempPath = t3lib_extMgm::extPath('t3users');
 require_once($tempPath.'services/ext_localconf.php');
