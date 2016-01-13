4132907d6ca626aef686e3a3f43fc7610cee862f
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2016-01-13 11:03:15 +0100

    AC#235-3373 mind. rn_base 1.0.0 nötig

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'DMK E-BUSINESS GmbH',
 	'constraints' => array(
 		'depends' => array(
-			'rn_base' => '0.8.0-',
+			'rn_base' => '1.0.0-',
 			'typo3' => '4.5.0-6.2.99',
 		),
 		'conflicts' => array(
