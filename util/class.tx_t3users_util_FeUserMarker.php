afa87ef223d40ac6ba8fbb1a1d6482a00d416b8a
Michael Wagner <michael.wagner@dmk-ebusiness.de>
2016-02-29 14:13:06 +0100

    is_current_user added to record for rendering and some codestyle changes

--- util/class.tx_t3users_util_FeUserMarker.php
+++ util/class.tx_t3users_util_FeUserMarker.php
@@ -1,36 +1,42 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2008 Rene Nitzsche (dev@dmk-ebusiness.de)
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ * Copyright notice
+ *
+ * (c) 2008-2016 DMK E-BUSINESS GmbH <dev@dmk-ebusiness.de>
+ * All rights reserved
+ *
+ * This script is part of the TYPO3 project. The TYPO3 project is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
-require_once(t3lib_extMgm::extPath('rn_base') . 'class.tx_rnbase.php');
 tx_rnbase::load('tx_rnbase_util_SimpleMarker');
 
-
 /**
- * Diese Klasse ist für die Erstellung von Markerarrays für FE User verantwortlich
+ * Repository to handle companies.
+ *
+ * @package TYPO3
+ * @subpackage tx_t3users
+ * @author Rene Nitzscher
+ * @author Michael Wagner
+ * @license http://www.gnu.org/licenses/lgpl.html
+ *          GNU Lesser General Public License, version 3 or later
  */
-class tx_t3users_util_FeUserMarker extends tx_rnbase_util_SimpleMarker {
-
+class tx_t3users_util_FeUserMarker
+	extends tx_rnbase_util_SimpleMarker
+{
 	/**
 	 * Initialisiert den Marker Array.
 	 * Optionen:
@@ -38,7 +44,7 @@ class tx_t3users_util_FeUserMarker extends tx_rnbase_util_SimpleMarker {
 	 * - hideuploads
 	 * @param array $options Hinweise an den Marker
 	 */
-	function __construct($options=false){
+	function __construct($options = FALSE) {
 		$this->options = is_array($options) ? $options : array();
 	}
 
@@ -46,54 +52,151 @@ class tx_t3users_util_FeUserMarker extends tx_rnbase_util_SimpleMarker {
 	 * Initialisiert die Labels für die Profile-Klasse
 	 *
 	 * @param tx_rnbase_util_FormatUtil $formatter
-	 * @param array $defaultMarkerArr
+	 * @param string $confId
+	 * @param mixed|array $defaultMarkerArr
+	 * @param string $marker
+	 *
+	 * @return array
 	 */
-	public function initLabelMarkers(&$formatter, $confId, $defaultMarkerArr = 0, $marker = 'FEUSER') {
-		return $this->prepareLabelMarkers('tx_t3users_models_feuser', $formatter, $confId, $defaultMarkerArr, $marker);
+	public function initLabelMarkers(
+		tx_rnbase_util_FormatUtil $formatter,
+		$confId,
+		$defaultMarkerArr = 0,
+		$marker = 'FEUSER'
+	) {
+		return $this->prepareLabelMarkers(
+			'tx_t3users_models_feuser',
+			$formatter,
+			$confId,
+			$defaultMarkerArr,
+			$marker
+		);
 	}
 
 	/**
-	 * @param string $template das HTML-Template
-	 * @param tx_t3users_models_feuser $feuser The fe user
-	 * @param $formatter der zu verwendente Formatter
+	 * Parses the template
+	 * @param string $template The HTML-Template
+	 * @param NULL|tx_t3users_models_feuser $feuser The fe user
+	 * @param tx_rnbase_util_FormatUtil $formatter The Formatte to use
 	 * @param string $confId Pfad der TS-Config des Objekt, z.B. 'listView.event.'
-	 * @param $marker Name des Markers für ein Object, z.B. FEUSER
+	 * @param string $marker Name des Markers für ein Object, z.B. FEUSER
 	 *        Von diesem String hängen die entsprechenden weiteren Marker ab: ###FEUSER_NAME###
 	 * @return String das geparste Template
 	 */
-	public function parseTemplate($template, &$feuser, &$formatter, $confId, $marker = 'FEUSER') {
-		if(!is_object($feuser)) {
+	public function parseTemplate(
+		$template,
+		$feuser,
+		tx_rnbase_util_FormatUtil $formatter,
+		$confId,
+		$marker = 'FEUSER'
+	) {
+		if (!is_object($feuser)) {
 			$feuser = self::getEmptyInstance('tx_t3users_models_feuser');
 		}
-		tx_rnbase_util_Misc::callHook('t3users','feuserMarker_initRecord',
-			array('item' => &$feuser, 'template'=>&$template, 'confid'=>$confId, 'marker'=>$marker, 'formatter'=>$formatter), $this);
-		$ignore = self::findUnusedCols($feuser->record, $template, $marker);
-		$markerArray = $formatter->getItemMarkerArrayWrapped($feuser->record, $confId , $ignore, $marker.'_',$feuser->getColumnNames());
+
+		tx_rnbase_util_Misc::callHook(
+			't3users',
+			'feuserMarker_initRecord',
+			array(
+				'item' => &$feuser,
+				'template' => &$template,
+				'confid' => $confId,
+				'marker' => $marker,
+				'formatter' => $formatter
+			),
+			$this
+		);
+
+		$this->prepareItem($feuser, $formatter->getConfigurations(), $confId);
+
+		// Es wird das MarkerArray mit den Daten des Records gefüllt.
+		$ignore = self::findUnusedCols($feuser->getRecord(), $template, $marker);
+		$markerArray = $formatter->getItemMarkerArrayWrapped(
+			$feuser->getRecord(),
+			$confId,
+			$ignore,
+			$marker . '_',
+			$feuser->getColumnNames()
+		);
+
 		// subparts erzeugen
 		$wrappedSubpartArray = $subpartArray = array();
-		$this->prepareSubparts($wrappedSubpartArray, $subpartArray, $template, $feuser, $formatter, $confId, $marker);
+		$this->prepareSubparts(
+			$wrappedSubpartArray,
+			$subpartArray,
+			$template,
+			$feuser,
+			$formatter,
+			$confId,
+			$marker
+		);
 
-		$this->prepareLinks($feuser, $marker, $markerArray, $subpartArray, $wrappedSubpartArray, $confId, $formatter, $template);
+		// Links erzeugen
+		$this->prepareLinks(
+			$feuser,
+			$marker,
+			$markerArray,
+			$subpartArray,
+			$wrappedSubpartArray,
+			$confId,
+			$formatter,
+			$template
+		);
 
 		// Gruppen hinzufügen
-		if($this->containsMarker($template, $marker.'_FEGROUPS'))
-			$template = $this->_addGroups($template, $feuser, $formatter, $confId.'group.', $marker.'_FEGROUP');
+		if ($this->containsMarker($template, $marker . '_FEGROUPS')) {
+			$template = $this->_addGroups($template, $feuser, $formatter, $confId . 'group.', $marker . '_FEGROUP');
+		}
 
-		tx_rnbase::load('tx_rnbase_util_Templates');
-		$out = tx_rnbase_util_Templates::substituteMarkerArrayCached($template, $markerArray, $subpartArray, $wrappedSubpartArray);
-		tx_rnbase_util_Misc::callHook('t3users','feuserMarker_afterSubst',
-			array('item' => &$feuser, 'template'=>&$out, 'confid'=>$confId, 'marker'=>$marker, 'formatter'=>$formatter), $this);
+		// das Template rendern
+		$out = self::substituteMarkerArrayCached($template, $markerArray, $subpartArray, $wrappedSubpartArray);
+
+		tx_rnbase_util_Misc::callHook(
+			't3users',
+			'feuserMarker_afterSubst',
+			array(
+				'item' => &$feuser,
+				'template' => &$out,
+				'confid' => $confId,
+				'marker' => $marker,
+				'formatter' => $formatter
+			),
+			$this
+		);
+
 		return $out;
+	}
+
+	/**
+	 * Führt vor dem parsen Änderungen am Model durch.
+	 *
+	 * @param Tx_Rnbase_Domain_Model_DomainInterface $item
+	 * @param tx_rnbase_configurations $configurations
+	 * @param string $confId
+	 *
+	 * @return void
+	 */
+	protected function prepareItem(
+		Tx_Rnbase_Domain_Model_RecordInterface $item,
+		tx_rnbase_configurations $configurations,
+		$confId
+	) {
+		$item->setIsCurrentUser(
+			$GLOBALS['TSFE']->fe_user->user['uid'] == $item->getUid()
+		);
+
+		parent::prepareItem($item, $configurations, $confId);
 	}
 
 	/**
 	 * Fügt den Sprecher in das Template ein.
 	 *
-	 * @param tx_t3users_models_feuser $feuser
 	 * @param string $template
+	 * @param tx_t3users_models_feuser $feuser
 	 * @param tx_rnbase_util_FormatUtil $formatter
 	 * @param string $confId
-	 * @param string $marker
+	 * @param string $markerPrefix
+	 *
 	 * @return string
 	 */
 	private function _addGroups($template, &$feuser, &$formatter, $confId, $markerPrefix) {
