1ebcf84560ffd39d5f5f6c2d1f34f685318c0e0f
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2018-05-14 14:59:16 +0200

    cleanup expect htmlspcialchars to be used

--- tests/actions/class.tx_t3users_tests_actions_Login_testcase.php
+++ tests/actions/class.tx_t3users_tests_actions_Login_testcase.php
@@ -138,7 +138,7 @@ class tx_t3users_tests_actions_Login_testcase extends tx_rnbase_tests_BaseTestCa
         $marker = $viewData->offsetGet('markers');
 
         $this->assertEquals(
-            tx_rnbase_util_Misc::getIndpEnv('TYPO3_SITE_URL') . '&#039;&gt;&lt;sc&lt;x&gt;ript&gt;alert(&quot;ohoh&quot;);&lt;/script&gt;&#039;',
+            tx_rnbase_util_Misc::getIndpEnv('TYPO3_SITE_URL') . '&#039;&amp;gt;&amp;lt;script&amp;gt;alert(&amp;quot;ohoh&amp;quot;);&amp;lt;/script&amp;gt;&#039;',
             $marker['redirect_url']
         );
     }
