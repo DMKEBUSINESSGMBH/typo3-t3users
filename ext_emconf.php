ffc8f4bfdb382edac77782f05b6c6f9963b876f2
Michael Wagner <michael.wagner@dmk-ebusiness.de>
2019-12-03 11:24:36 +0100

    drop typo3 4 support and add typo3 9 support to emconf
    #7

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'rn_base' => '1.4.0-',
-            'typo3' => '4.5.0-8.7.99',
+            'typo3' => '6.2.14-9.5.99',
         ),
         'conflicts' => array(),
         'suggests' => array(
